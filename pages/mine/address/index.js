911619daca4056e19cbba69e882cd881a41fa763
myfCode <mayuefeng90@126.com>
2018-07-27 11:03:57 +0800

    我的地址修改

--- pages/mine/address/index.js
+++ pages/mine/address/index.js
@@ -12,11 +12,11 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        // this.init()
+        this.init()
     },
 
     onShow(){
-      this.init()
+      // this.init()
     },
 
     init(){
