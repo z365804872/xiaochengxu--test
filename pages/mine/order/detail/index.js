482b1d3d1b399c350ab4d39d907e22f4f42e9694
myfCode <mayuefeng90@126.com>
2018-10-11 23:02:59 +0800

    修改

--- pages/mine/order/detail/index.js
+++ pages/mine/order/detail/index.js
@@ -73,7 +73,7 @@ Page({
         let that = this
         let {type, orderState, state, orderId, buyOrSellId} = that.data
 
-        if(type == 1 && orderState == 2 && state == 0){
+        if(type == 1 && orderState == 2 && (state == 0 || state == 7)){
             wx.showModal({
                 content: '是否取消订单？',
                 confirmText: '确认',
