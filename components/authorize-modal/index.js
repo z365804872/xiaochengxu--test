d6d7d4e07bbe385e7f8106019a0ebb0a042e52a7
myfCode <mayuefeng90@126.com>
2018-10-27 13:41:03 +0800

    砍价

---
+++ components/authorize-modal/index.js
@@ -0,0 +1,28 @@
+// components/action/action.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        show: {
+            type: Boolean,
+            value: false
+        },
+
+        type: String
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {},
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        getUserInfo(e){
+            wx.showToast({content: 'dfafaß'})
+        }
+    }
+})
